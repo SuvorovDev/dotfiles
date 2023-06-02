be71c5599e91633d70d0542ce4249fa42f74ebb1
Suvorov Mikhail <mr.plaim@gmail.com>
2023-06-02 20:48:04 +0300

    Update lf & fish

---
+++ .config/fish/aliases.fish
@@ -0,0 +1,17 @@
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+end
+
+alias	cp "cp -iv"
+alias	mv "mv -iv"
+alias	rm "rm -vI"
+
+alias yt "yt-dlp --embed-metadata -i -S ext:mp4:m4a"
+alias yta "yt-dlp --embed-metadata -i -x -f bestaudio/best"
+alias ytt "yt-dlp --embed-metadata -i --skip-download --write-thumbnail"
+
+alias img "chafa"
+
+alias x "startx"
+alias pacman "sudo pacman"
