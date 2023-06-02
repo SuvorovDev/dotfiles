0a3b5b88b701922b349be7089ebc25c15017c755
Suvorov Mikhail <mr.plaim@gmail.com>
2023-06-03 01:21:43 +0300

    lf upgrade

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,23 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     neofetch
 end
+
+#lfcd
+function lfcd
+    set tmp (mktemp)
+    # `command` is needed in case `lfcd` is aliased to `lf`
+    command lf -last-dir-path=$tmp $argv
+    if test -f "$tmp"
+        set dir (cat $tmp)
+        rm -f $tmp
+        if test -d "$dir"
+            if test "$dir" != (pwd)
+                cd $dir
+            end
+        end
+    end
+end
+
+bind \co 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
+
+
